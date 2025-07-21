cdabd9ce9db79893321a6d3def9a787c954118e4
Muuscodes <muuscodes@gmail.com>
2025-07-20 18:28:00 -0600

    Syntax error

--- src/DecryptedText.tsx
+++ src/DecryptedText.tsx
@@ -57,6 +57,7 @@ export default function DecryptedText({
   );
   const [hasAnimated, setHasAnimated] = useState<boolean>(false);
   const containerRef = useRef<HTMLSpanElement | null>(null);
+  console.log(isScrambling, revealedIndices);
 
   useEffect(() => {
     let interval: NodeJS.Timeout | undefined;
